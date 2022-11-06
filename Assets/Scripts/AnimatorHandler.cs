9d52e126f43363d1eaaed2f17633144a006d9235
lolmasterdeluxe <kiddrifdi@gmail.com>
2022-11-07 02:42:11 +0800

    Revamping game
    updating to soulslike

---
+++ Assets/Scripts/AnimatorHandler.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IG
+{
+    public class AnimatorHandler : MonoBehaviour
+    {
+        public Animator anim;
+        private int vertical;
+        private int horizontal;
+        public bool canRotate;
+
+        public void Initialize()
+        {
+            anim = GetComponent<Animator>();
+            vertical = Animator.StringToHash("Vertical");
+            horizontal = Animator.StringToHash("Horizontal");
+
+        }
+
+        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement)
+        {
+            #region Vertical
+            float v = 0;
+
+            if (verticalMovement > 0 && verticalMovement < 0.55f)
+            {
+                v = 0.5f;
+            }
+            else if (verticalMovement > 0.55f)
+            {
+                v = 1;
+            }
+            else if (verticalMovement < 0 && verticalMovement > -0.55f)
+            {
+                v = -0.5f;
+            }
+            else if (verticalMovement < -0.55f)
+            {
+                v = -1f;
+            }
+            else
+            {
+                v = 0;
+            }
+
+            #endregion
+
+            #region Horizontal
+            float h = 0;
+
+            if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+            {
+                h = 0.5f;
+            }
+            else if (horizontalMovement > 0.55f)
+            {
+                h = 1;
+            }
+            else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+            {
+                h = -0.5f;
+            }
+            else if (horizontalMovement < -0.55f)
+            {
+                h = -1f;
+            }
+            else
+            {
+                h = 0;
+            }
+            #endregion
+
+            anim.SetFloat(vertical, v, 0.1f, Time.deltaTime);
+            anim.SetFloat(horizontal, h, 0.1f, Time.deltaTime);
+        }
+
+        public void CanRotate()
+        {
+            canRotate = true;
+        }
+
+        public void StopRotate()
+        {
+            canRotate = false;
+        }
+    }
+}
