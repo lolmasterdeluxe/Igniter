91432db3b7adb795dae98bc93090250aaf615299
lolmasterdeluxe <kiddrifdi@gmail.com>
2022-12-12 13:26:18 +0800

    updated dungeon layers and added navmesh surface

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.AI.Navigation;
 
 namespace IG
 {
@@ -19,12 +20,19 @@ namespace IG
         private InputHandler inputHandler;
         [SerializeField]
         private DungeonManager dungeonManager;
+        public NavMeshSurface surface;
+
+        private void Awake()
+        {
+            surface = GetComponent<NavMeshSurface>();
+        }
 
         public void StartGame()
         {
             dungeonManager.gameObject.SetActive(true);
             lobby.SetActive(false);
             dungeonManager.CreateDungeonLayout();
+            surface.BuildNavMesh();
             player.transform.position = dungeonSpawnPoint.position;
             player.transform.rotation = dungeonSpawnPoint.rotation;
         }
@@ -34,6 +42,7 @@ namespace IG
             dungeonManager.ClearDungeonLayout();
             dungeonManager.gameObject.SetActive(false);
             lobby.SetActive(true);
+            surface.RemoveData();
             player.transform.position = lobbySpawnPoint.position;
             player.transform.rotation = lobbySpawnPoint.rotation;
         }
